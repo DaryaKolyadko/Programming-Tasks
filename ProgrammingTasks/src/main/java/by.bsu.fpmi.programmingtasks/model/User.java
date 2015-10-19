1b5a6398b422707012d43223b49eb26804a2d378
darya_kolyadko <skvoznak1996@gmail.com>
2015-10-19 06:18:50 +0300

    add class User (only necessary fields) and maven dependencies (spring framework)

---
+++ ProgrammingTasks/src/main/java/by.bsu.fpmi.programmingtasks/model/User.java
@@ -0,0 +1,12 @@
+package by.bsu.fpmi.programmingtasks.model;
+
+/**
+ * Created by Даша on 19.10.2015.
+ */
+public class User
+{
+    public String id;
+    public String userName;
+    public String passwordHash;
+    public int rating; // рейтинг
+}
